04d1fc0f748ef87988ea77c5def1b86a5de459ad
busingepius <busingepius@gmail.com>
2024-05-18 00:29:53 -0500

    3.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:d/bloc/count.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,115 +14,153 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'D Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BlocProvider(
+        lazy: true,
+        create: (context) => CounterCubit(),
+        child: MyHomePage(title: 'Home'),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class MyHomePage extends StatelessWidget {
   final String title;
 
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("$title ${BlocProvider.of<CounterCubit>(context).state}"),
+      ),
+      body: BlocBuilder<CounterCubit, int>(
+        builder: (context, count) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '${count}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Increment',
+        onPressed: () {
+          context.read<CounterCubit>().increment();
+          print("-----------------------------------------------------------");
+          print(
+              "${context.read<CounterCubit>().state} dhgywgywguwgwytgwywtgytwytyw");
+
+          Navigator.push(context, MaterialPageRoute(builder: (_) {
+            print(
+                "${BlocProvider.of<CounterCubit>(context).runtimeType} its type is CounterCubit dhgywgywguwgwytgwywtgytwytyw");
+            // print(context);
+            return AnotherTextWidget(
+                context: context, key: const PageStorageKey("here"));
+          }));
+        },
+        child: const Icon(Icons.add),
+      ), // T
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class AnotherTextWidget extends StatefulWidget {
+  BuildContext context;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  AnotherTextWidget({Key? key, required this.context}) : super(key: key);
+
+  @override
+  State<AnotherTextWidget> createState() => _AnotherTextWidgetState();
+}
+
+class _AnotherTextWidgetState extends State<AnotherTextWidget> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    BlocProvider.of<CounterCubit>(widget.context);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    BlocProvider.of<CounterCubit>(widget.context);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return BlocProvider<CounterCubit>.value(
+      value: BlocProvider.of<CounterCubit>(widget.context),
+      child: Scaffold(
+        key: const PageStorageKey("Here"),
+        body: SafeArea(
+          child: Column(
+            children: [
+              BlocBuilder<CounterCubit, int>(builder: (bloc, state) {
+                return Text("$state");
+              }),
+              // ,
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            BlocProvider.of<CounterCubit>(widget.context).increment();
+            Navigator.push(
+              widget.context,
+              MaterialPageRoute(
+                builder: (_) => ThirdScreen(context: widget.context),
+              ),
+            );
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class ThirdScreen extends StatelessWidget {
+  BuildContext context;
+
+  ThirdScreen({required this.context, super.key});
+
+  @override
+  Widget build(BuildContext _) {
+    return Scaffold(
+        body: BlocProvider<CounterCubit>.value(
+      value: BlocProvider.of<CounterCubit>(context),
+      child: BlocBuilder<CounterCubit, int>(builder: (bloc, state) {
+        return Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            children: [
+              Text("$state"),
+              OutlinedButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text("BACK")),
+            ],
+          ),
+        );
+      }),
+    ));
   }
 }
